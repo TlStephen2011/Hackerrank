ec81b0391a69c28fe2e787781435a8ae84060da4
TlStephen2011 <tlstephen11@gmail.com>
2017-12-17 17:24:02 +0200

    base code for red knight challenge

---
+++ Red Knight's Shortest Path/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void printShortestPath(int n, int i_start, int j_start, int i_end, int j_end) {
+  //  Print the distance along with the sequence of moves.
+}
+
+int main() {
+  int n;
+  cin >> n;
+  int i_start;
+  int j_start;
+  int i_end;
+  int j_end;
+  cin >> i_start >> j_start >> i_end >> j_end;
+  printShortestPath(n, i_start, j_start, i_end, j_end);
+  return 0;
+}
